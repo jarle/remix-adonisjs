9dce7e96140c511d0fa00b9fa87e12c7d96f2ccb
Jarle Mathiesen <jarle@users.noreply.github.com>
2024-08-16 16:41:07 +0200

    Respect Vite base config (#65)

--- packages/reference-app/vite.config.ts
+++ packages/reference-app/vite.config.ts
@@ -1,17 +1,12 @@
-import adonisjs from '@adonisjs/vite/client'
 import { vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ isSsrBuild }) => ({
-  base: '/assets/',
   plugins: [
     remix({
       appDirectory: 'resources/remix_app',
       buildDirectory: 'build/remix',
       serverBuildFile: 'server.js',
-    }),
-    adonisjs({
-      entrypoints: [],
     }),
   ],
   optimizeDeps: {
