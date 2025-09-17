684692aaa7d95e7eacb39fbec7bc61d9d8f76df6
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2025-09-17 20:12:31 +0200

    remove examples folder

--- examples/with-tailwind-and-shadcn/env.d.ts
+++
@@ -1,5 +0,0 @@
-import type { AdonisApplicationContext } from '@matstack/remix-adonisjs/types';
-
-declare module 'react-router' {
-  interface AppLoadContext extends AdonisApplicationContext { }
-}
--- examples/with-tailwind-and-shadcn/react-router.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from '@react-router/dev/config'
-export default {
-  ssr: true,
-  appDirectory: 'resources/remix_app',
-  buildDirectory: 'build/remix',
-  serverBuildFile: 'server.js',
-} satisfies Config
